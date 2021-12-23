70396521526917c5842c0e2091c3b4e8b78df059
nareshkumar.pasham@gmail.com <nareshkumar.pasham@gmail.com>
2021-12-23 22:53:59 +0530

    refactored code

--- src/com/code/challenge/BinaryTree.java
+++ src/com/code/challenge/BinaryTree.java
@@ -8,64 +8,63 @@ package com.code.challenge;
  *
  */
 public class BinaryTree {
-	
+
 	Node root;
-	
+
 	public void addNode(int key, String name) {
-		
-		Node newNode= new Node(key, name);
-		
-		if(root==null) { // Step 1: When root node is null
-			root=newNode; /* 1.1 : When root node is null assign it as rootNode*/
-		}else { //Step 2 : When root Node is not null
+
+		Node newNode = new Node(key, name);
+
+		if (root == null) { // Step 1: When root node is null
+			root = newNode; /* 1.1 : When root node is null assign it as rootNode */
+		} else { // Step 2 : When root Node is not null
 			Node focusNode = root;// 2.1: make root node as focus node
 			Node parent;
-			while(true){ 
-				parent = focusNode; //2.1.1 : Make focus node as parent node
-				
-				if(key < focusNode.key) {//
-					
+			while (true) {
+				parent = focusNode; // 2.1.1 : Make focus node as parent node
+
+				if (key < focusNode.key) {//
+
 					focusNode = focusNode.leftChild;
-					
-					if(focusNode == null) {
+
+					if (focusNode == null) {
 						parent.leftChild = newNode;
 						return;
 					}
-					
-				}
-				else {
+
+				} else {
 					focusNode = focusNode.rightChild;
-					if(focusNode == null) {
+					if (focusNode == null) {
 						parent.rightChild = newNode;
 						return;
 					}
-					
+
 				}
-				
+
 			}
 		}
-		
+
 	}
-	
+
 	/*
 	 * InOrder traverse --small to big number
 	 */
 
 	public void inOrderTraversal(Node focusNode) {
-		if(focusNode != null) {
-			
+		if (focusNode != null) {
+
 			inOrderTraversal(focusNode.leftChild);
 			System.out.println(focusNode);
 			inOrderTraversal(focusNode.rightChild);
-			
+
 		}
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BinaryTree tree=new BinaryTree();
+		BinaryTree tree = new BinaryTree();
 		tree.addNode(50, "Boss");
 		tree.addNode(10, "Reception");
 		tree.addNode(25, "Manager sales");
@@ -74,21 +73,18 @@ public class BinaryTree {
 		tree.addNode(45, "Security staff");
 		tree.addNode(60, "CEO");
 		tree.inOrderTraversal(tree.root);
-		
-		
 
 	}
 
 }
 
-
-class Node{
+class Node {
 	int key;
 	String name;
-	
+
 	Node leftChild;
 	Node rightChild;
-	
+
 	public Node(int key, String name) {
 		super();
 		this.key = key;
@@ -99,6 +95,5 @@ class Node{
 	public String toString() {
 		return "Node [key=" + key + ", name=" + name + "]";
 	}
-	
-	
+
 }
